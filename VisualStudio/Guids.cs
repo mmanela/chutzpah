01932afec2f12a0883eeb463e27b51d2a8545748
Matthew Manela <none@none>
2011-07-11 17:27:29 -0700

    cleanup and renames
    --HG--
    rename : Chutzpah/TestResultBuilder.cs => Chutzpah/TestResultsBuilder.cs

--- VisualStudio/Guids.cs
+++ VisualStudio/Guids.cs
@@ -8,13 +8,7 @@ namespace Chutzpah.VisualStudio
 
 
         public const string guidChutzpahCmdSetString = "F0D5D16A-56CE-40A2-9C34-B890D6961CF2";
-        //public const string guidSourceEditorCmdSetString = "F683BB14-1D01-444C-9B19-C40485C67824";
-        //public const string guidSolutionItemCmdSetString = "3AC35B4C-E12D-40EE-9BCA-33F5B6B4191E";
-        //public const string guidSolutionFolderNodeCmdSetString = "7E174869-EE8C-45FC-BEF4-5D19B6AF146D";
-
         public static readonly Guid guidChutzpahCmdSet = new Guid(guidChutzpahCmdSetString);
-        //public static readonly Guid guidSourceEditorCmdSet = new Guid(guidSourceEditorCmdSetString);
-        //public static readonly Guid guidSolutionItemCmdSet = new Guid(guidSolutionItemCmdSetString);
-        //public static readonly Guid guidSolutionFolderNodeCmdSet = new Guid(guidSolutionFolderNodeCmdSetString);
+
     }
 }
