305336c60a439233e62abc672d145eab45425e52
mmanela <none@none>
2012-03-13 09:00:29 -0700

    update version number

--- VS11.Plugin/Properties/AssemblyInfo.cs
+++ VS11.Plugin/Properties/AssemblyInfo.cs
@@ -28,6 +28,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.3.2.78")]
-[assembly: AssemblyVersion("1.3.2.78")]
-[assembly: AssemblyFileVersion("1.3.2.78")]
+// [assembly: AssemblyVersion("1.4.0.79")]
+[assembly: AssemblyVersion("1.4.0.79")]
+[assembly: AssemblyFileVersion("1.4.0.79")]
