85113d07639799a6407e31cbb52054ececd57779
mmanela <none@none>
2012-03-26 17:56:46 -0700

    41 expected and actual result are not shown in Visual Studio

--- VS11.Plugin/ChutzpahExtensionMethods.cs
+++ VS11.Plugin/ChutzpahExtensionMethods.cs
@@ -13,7 +13,7 @@ namespace VS11.Plugin
             return new Microsoft.VisualStudio.TestPlatform.ObjectModel.TestCase(BuildFullyQualifiedName(testCase), Constants.ExecutorUri, testCase.InputTestFile)
 			{
 				CodeFilePath = testCase.InputTestFile,
-				DisplayName = testCase.ModuleName + " " + testCase.TestName,
+                DisplayName = GetTestDisplayText(testCase),
 				LineNumber = testCase.Line,
 			};
 		}
@@ -24,7 +24,7 @@ namespace VS11.Plugin
 			return new Microsoft.VisualStudio.TestPlatform.ObjectModel.TestResult(testCase)
 			{
 				DisplayName = testCase.DisplayName,
-				ErrorMessage = result.Message,
+                ErrorMessage = GetTestFailureMessage(result),
 				Outcome = result.ToVsTestOutcome()
 			};
 		}
@@ -38,6 +38,26 @@ namespace VS11.Plugin
         {
             var parts = new[] { testCase.ModuleName, testCase.TestName, testCase.InputTestFile}.Where(x => !String.IsNullOrEmpty(x));
             return String.Join("::", parts);
+        }
+
+        private static string GetTestDisplayText(Chutzpah.Models.TestCase testCase)
+        {
+            return string.IsNullOrWhiteSpace(testCase.ModuleName) ? testCase.TestName : string.Format("{0} {1}", testCase.ModuleName, testCase.TestName);
+        }
+
+        private static string GetTestFailureMessage(Chutzpah.Models.TestResult result)
+        {
+            var errorString = "";
+            if (result.Expected != null || result.Actual != null)
+            {
+                errorString += string.Format("Expected: {0}, Actual: {1}", result.Expected, result.Actual);
+            }
+            else if (!string.IsNullOrWhiteSpace(result.Message))
+            {
+                errorString += string.Format("{0}", result.Message);
+            }
+
+            return errorString;
         }
 	}
 }
