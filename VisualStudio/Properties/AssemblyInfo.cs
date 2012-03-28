d298d745683394849c77ebef48a8c57847cb1255
mmanela <none@none>
2012-03-28 08:46:10 -0700

    update version to 1.4.1

--- VisualStudio/Properties/AssemblyInfo.cs
+++ VisualStudio/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.4.0.81")]
+[assembly: AssemblyVersion("1.4.1.86")]
 
 
 
