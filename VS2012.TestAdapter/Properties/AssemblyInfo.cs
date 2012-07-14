fc5c86508abd82078980282f62019538d0e25e8c
mmanela <none@none>
2012-07-13 22:39:14 -0700

    fix missing command line text

--- VS2012.TestAdapter/Properties/AssemblyInfo.cs
+++ VS2012.TestAdapter/Properties/AssemblyInfo.cs
@@ -28,6 +28,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("2.0.0.136")]
-[assembly: AssemblyVersion("2.0.0.136")]
-[assembly: AssemblyFileVersion("2.0.0.136")]
+// [assembly: AssemblyVersion("2.0.0.137")]
+[assembly: AssemblyVersion("2.0.0.137")]
+[assembly: AssemblyFileVersion("2.0.0.137")]
