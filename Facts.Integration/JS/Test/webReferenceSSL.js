a80c6c8addabdeff536c748f49e87de079c3b00a
mmanela <none@none>
2012-08-08 08:14:17 -0700

    add integration test for referencing ssl urls

---
+++ Facts.Integration/JS/Test/webReferenceSSL.js
@@ -0,0 +1,8 @@
+﻿/// <reference path="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" />
+
+$(function(){
+
+    test("A jquery test", function () {
+      ok($.inArray(2,[1,2,3]), "this test is fine");
+  });
+});
