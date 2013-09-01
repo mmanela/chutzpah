fb8be5c9a9c8431c0a07a15aabd0c060b7648c80
mmanela <mmanela@MMANELA.redmond.corp.microsoft.com>
2013-08-31 22:12:02 -0700

    v2.5.0

--- Chutzpah/Properties/AssemblyInfo.cs
+++ Chutzpah/Properties/AssemblyInfo.cs
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.4.3.281")]
+[assembly: AssemblyVersion("2.5.0.0")]
