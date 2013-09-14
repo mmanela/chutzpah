4717912004a2299272761bd76cc50838380ab763
Patrick Hallisey <hallipr@gmail.com>
2013-09-14 00:30:18 -0500

    Mocha runner finally runs

--- Chutzpah/JSRunners/mochaRunner.js
+++ Chutzpah/JSRunners/mochaRunner.js
@@ -1,131 +1,97 @@
-﻿/// <reference path="chutzpahRunner.js" />
-/// <reference path="mocha.js" />
-/*globals phantom, chutzpah, window, mocha*/
-
-(function() {
+﻿(function () {
     'use strict';
-    phantom.injectJs('chutzpahRunner.js');
-
-    var startTime = null;
-    var activeTestCase = null;
-    
-    chutzpah.isTestingFinished = false;
-    chutzpah.testCases = [];
-    
-    var passed = 0;
-    var failed = 0;
-    var skipped = 0;
-
-    var mocha;
-
-    function log(obj) {
-        console.log(JSON.stringify(obj));
-    }
-    
-    function logCoverage() {
-        if (window._Chutzpah_covobj_name && window[window._Chutzpah_covobj_name]) {
-            log({ type: "CoverageObject", object: window[window._Chutzpah_covobj_name] });
-        }
-    }
-    
-    var chutzpahMochaReporter = function(runner) {
-        runner.on('start', function() {
-            startTime = new Date();
-            
-            log({ type: "FileStart" });
-        });
-        
-        runner.on('end', function() {
-            logCoverage();
-
-            log({
-                type: "FileDone", 
-                timetaken: new Date() - startTime, 
-                passed: passed, 
-                failed: failed
-            });           
-
-            chutzpah.isTestingFinished = true;
-        });
-        
-        runner.on('suite', function(suite) {
-            chutzpah.currentModule = suite.fullTitle();
-        });
-        
-        runner.on('suite end', function(suite) {
-            chutzpah.currentModule = null;
-        });
-        
-        runner.on('test', function(test) {
-             activeTestCase = {
-                 moduleName: chutzpah.currentModule, 
-                 testName: test.title, 
-                 testResults: []
-             };
-             chutzpah.testCases.push(activeTestCase);
-             log({ type: "TestStart", testCase: activeTestCase });
-        });
-        
-        runner.on('test end', function(test) {
-            activeTestCase.timetaken = test.duration;
-
-            log({ type: "TestDone", testCase: activeTestCase });
-        });
-        
-        //runner.on('hook', function(hook) { });
-        //runner.on('hook end', function(hook) { });
-        
-        runner.on('pass', function(test) {
-             passed++;
-        });
-        
-        runner.on('fail', function(test, err) {
-             failed++;
-        });
-        
-        runner.on('pending', function(test) {
-             skipped++;
-        });
-    };
 
     function onInitialized() {
-        console.log("!!_!! onInitialized");
-
-        var oldOnLoad = window.onload;
-
-        window.onload = function () {
-            if (oldOnLoad) {
-                oldOnLoad();
-            }
-
-            log("!!_!! Starting Mocha...");
-
-            var Mocha = require('mocha');
-            mocha = new Mocha({ reporter: chutzpahMochaReporter });
-        };
+        chutzpah.isTestingFinished = false;
+        chutzpah.testCases = [];
     }
 
     function onPageLoaded() {
-        console.log("!!_!! onPageLoaded");
     }
 
     function isMochaLoaded() {
-        console.log("!!_!! isMochaLoaded");
-        
-        return mocha != null;
+        return typeof global != 'undefined' && typeof global.mocha != 'undefined';
     }
 
     function onMochaLoaded() {
-        console.log("!!_!! onMochaLoaded");
-
-        chutzpah.isTestingFinished = false;
-        chutzpah.testCases = [];
+        function log(obj) {
+            console.log(JSON.stringify(obj));
+        }
 
         if (chutzpah.testMode === 'discovery') {
-            
+            // not quite sure here.
         }
-        
-        mocha.run();
+
+        var chutzpahMochaReporter = function (runner) {
+            var startTime = null,
+                activeTestCase = null,
+                passed = 0,
+                failed = 0,
+                skipped = 0;
+
+
+            runner.on('start', function () {
+                startTime = new Date();
+
+                log({ type: "FileStart" });
+            });
+
+            runner.on('end', function () {
+                if (window._Chutzpah_covobj_name && window[window._Chutzpah_covobj_name]) {
+                    log({ type: "CoverageObject", object: window[window._Chutzpah_covobj_name] });
+                }
+
+                log({
+                    type: "FileDone",
+                    timetaken: new Date() - startTime,
+                    passed: passed,
+                    failed: failed
+                });
+
+                chutzpah.isTestingFinished = true;
+            });
+
+            runner.on('suite', function (suite) {
+                chutzpah.currentModule = suite.fullTitle();
+            });
+
+            runner.on('suite end', function (suite) {
+                chutzpah.currentModule = null;
+            });
+
+            runner.on('test', function (test) {
+                activeTestCase = {
+                    moduleName: chutzpah.currentModule,
+                    testName: test.title,
+                    testResults: []
+                };
+                chutzpah.testCases.push(activeTestCase);
+                log({ type: "TestStart", testCase: activeTestCase });
+            });
+
+            runner.on('test end', function (test) {
+                activeTestCase.timetaken = test.duration;
+
+                log({ type: "TestDone", testCase: activeTestCase });
+            });
+
+            //runner.on('hook', function(hook) { });
+            //runner.on('hook end', function(hook) { });
+
+            runner.on('pass', function (test) {
+                passed++;
+            });
+
+            runner.on('fail', function (test, err) {
+                failed++;
+            });
+
+            runner.on('pending', function (test) {
+                skipped++;
+            });
+        };
+
+        window.mocha.setup({ reporter: chutzpahMochaReporter });
     }
 
     function isTestingDone() {
@@ -133,6 +99,7 @@
     }
 
     try {
+        phantom.injectJs('chutzpahRunner.js');
         chutzpah.runner(onInitialized, onPageLoaded, isMochaLoaded, onMochaLoaded, isTestingDone);
     } catch (e) {
         phantom.exit(2); // Unkown error
