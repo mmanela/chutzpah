9ea6e3154b2d28598ce8986d187135153ebb0e6b
mmanela <mmanela@gmail.com>
2013-12-17 20:41:09 -0800

    add full set of polyfills for typescript

--- Chutzpah/Compilers/TypeScript/polyfill.js
+++ Chutzpah/Compilers/TypeScript/polyfill.js
@@ -120,8 +120,78 @@ if (!Array.prototype.forEach) {
     };
 }
 
+if ('function' !== typeof Array.prototype.reduce) {
+    Array.prototype.reduce = function (callback, opt_initialValue) {
+        'use strict';
+        if (null === this || 'undefined' === typeof this) {
+            // At the moment all modern browsers, that support strict mode, have
+            // native implementation of Array.prototype.reduce. For instance, IE8
+            // does not support strict mode, so this check is actually useless.
+            throw new TypeError(
+                'Array.prototype.reduce called on null or undefined');
+        }
+        if ('function' !== typeof callback) {
+            throw new TypeError(callback + ' is not a function');
+        }
+        var index, value,
+            length = this.length >>> 0,
+            isValueSet = false;
+        if (1 < arguments.length) {
+            value = opt_initialValue;
+            isValueSet = true;
+        }
+        for (index = 0; length > index; ++index) {
+            if (this.hasOwnProperty(index)) {
+                if (isValueSet) {
+                    value = callback(value, this[index], index, this);
+                }
+                else {
+                    value = this[index];
+                    isValueSet = true;
+                }
+            }
+        }
+        if (!isValueSet) {
+            throw new TypeError('Reduce of empty array with no initial value');
+        }
+        return value;
+    };
+}
+
+if (!Array.prototype.filter) {
+    Array.prototype.filter = function (fun /*, thisp */) {
+        "use strict";
+
+        if (this == null)
+            throw new TypeError();
+
+        var t = Object(this);
+        var len = t.length >>> 0;
+        if (typeof fun != "function")
+            throw new TypeError();
+
+        var res = [];
+        var thisp = arguments[1];
+        for (var i = 0; i < len; i++) {
+            if (i in t) {
+                var val = t[i]; // in case fun mutates this
+                if (fun.call(thisp, val, i, t))
+                    res.push(val);
+            }
+        }
+
+        return res;
+    };
+}
+
 if (!String.prototype.trim) {
     String.prototype.trim = function () {
         return this.replace(/^\s+|\s+$/gm, '');
+    };
+}
+
+if (!Date.now) {
+    Date.now = function now() {
+        return new Date().getTime();
     };
 }
