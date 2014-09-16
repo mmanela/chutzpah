3f3bc7f9621c844b942477b0484ff7c249d5c7fb
mmanela <mmanela@gmail.com>
2014-09-15 22:17:19 -0700

    change chocolatey url

--- chocolateyInstall.ps1
+++ chocolateyInstall.ps1
@@ -1,1 +1,1 @@
-Install-ChocolateyZipPackage 'chutzpah' 'https://chutzpah.codeplex.com/downloads/get/811915'  "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage 'chutzpah' 'https://github.com/mmanela/chutzpah/releases/download/v3.2.3/Chutzpah.3.2.3.zip'  "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
