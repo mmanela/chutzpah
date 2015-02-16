96b8911b79ad284f4c2d543a71d561cd668ba16d
Matthew Manela <mmanela@gmail.com>
2015-02-15 21:58:58 -0800

    Validate functional tests when forcing batching mode

--- Facts.Integration/JS/Test/SubFolder/SubSubFolder/test-jasmine.js
+++
@@ -1,9 +0,0 @@
-﻿/// <reference path="../../../code/code.js" />
-
-describe("general", function () {
-    it("A basic test", function () {
-        expect(true).toBeTruthy();
-        var value = "hello";
-        expect("hello").toEqual(value);
-    });
-});
--- Facts.Integration/JS/Test/SubFolder/SubSubFolder/test-qunit2.js
+++ Facts.Integration/JS/Test/SubFolder/SubSubFolder/test-qunit2.js
@@ -1,7 +1,7 @@
 ﻿/// <reference path="../../../code/code.js" />
 
- test("A basic test", function () {
+ test("A basic test 2", function () {
       ok(true, "this test is fine");
       var value = "hello";
-      equals("hello", value, "We expect value to be hello");
+      equal("hello", value, "We expect value to be hello");
   });
