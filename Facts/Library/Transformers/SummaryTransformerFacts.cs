baeba5a0be2ba4b84d35f0c45c109b4568b3eaf8
James Telfer <james@telfer.com>
2015-09-21 18:18:29 +1000

    Remove BOM from transformed result output
    Fixes #411. The BOM causes errors when read by many Java libraries,
    including those used by Jenkins. Removing it makes it painless to
    integrate.

---
+++ Facts/Library/Transformers/SummaryTransformerFacts.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Chutzpah.Models;
+using Chutzpah.Transformers;
+using Xunit;
+using Chutzpah.Wrappers;
+using Moq;
+using System.Text;
+
+namespace Chutzpah.Facts.Library.Transformers
+{
+    public class SummaryTransformerFacts
+    {
+        [Fact]
+        public void Will_write_utf8_without_byte_order_mark()
+        {
+            Encoding summaryEncoding = null;
+
+            var fsw = new Mock<IFileSystemWrapper>();
+            fsw.Setup(f => f.WriteAllText(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<Encoding>()))
+                .Callback<string, string, Encoding>((path, text, enc) => { summaryEncoding = enc; })
+                .Verifiable();
+
+            var sut = new TestSummaryTransformer(fsw.Object);
+
+            sut.Transform(new TestCaseSummary(), "outputfile");
+
+            fsw.Verify(); // Write called
+            Assert.NotNull(summaryEncoding); // encoding captured
+            Assert.Empty(summaryEncoding.GetPreamble()); // encoding has no BOM preamble
+            Assert.Empty(summaryEncoding.GetBytes(""));  // encoding does not write leading bytes to output
+        }
+
+        private class TestSummaryTransformer : SummaryTransformer
+        {
+            public TestSummaryTransformer(IFileSystemWrapper wrapper) : base(wrapper)
+            {
+            }
+
+            public override string Name
+            {
+                get { return "Test"; }
+            }
+
+            public override string Description
+            {
+                get { return "Test"; }
+            }
+
+            public override string Transform(TestCaseSummary testFileSummary)
+            {
+                return "";
+            }
+        }
+    }
+}
