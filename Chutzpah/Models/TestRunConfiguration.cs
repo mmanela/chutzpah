1fa9a624d332d7005e7d119c1a73536c67c96b9f
Matthew Manela <mmanela@gmail.com>
2016-01-30 15:51:32 -0800

    Add testrunconfiguration class

---
+++ Chutzpah/Models/TestRunConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Chutzpah.Models
+{
+    public class TestRunConfiguration
+    {
+        public List<List<PathInfo>> TestGroups { get; set; }
+
+        public int? MaxDegreeOfParallelism { get; set; }
+
+        public bool EnableTracing { get; set; }
+        public string TraceFilePath { get; set; }
+
+        public TestRunConfiguration()
+        {
+            TestGroups = new List<List<PathInfo>>();
+            TraceFilePath = Path.Combine(Path.GetTempPath(), Constants.LogFileName);
+        }
+
+    }
+}
