44c961b20eb1c56fca149eaea5020d71789b52ad
Matthew Manela <mmanela@gmail.com>
2016-02-03 21:04:04 -0800

    update gitignore

--- qt.bat
+++ qt.bat
@@ -1,12 +1,5 @@
-rem Quickly execute Chutzpah on an integration test file
+rem Quickly execute Chutzpah
 
 @echo off
-echo Runing chutzpah on a integration test file named .\Facts.Integration\js\Test\%1
 
-IF ""=="%1" goto end
-
-.\ConsoleRunner\bin\Debug\chutzpah.console.exe .\Facts.Integration\js\Test\%1 /debug
-goto end
-
-
-:end
+.\ConsoleRunner\bin\Debug\chutzpah.console.exe %*
