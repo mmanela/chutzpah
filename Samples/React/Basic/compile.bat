9d3441faf921f089ee4a1476de0986c92c6de56c
Matthew Manela <mmanela@gmail.com>
2017-09-15 15:54:33 -0700

    revert

--- Samples/React/Basic/compile.bat
+++ Samples/React/Basic/compile.bat
@@ -1,3 +1,3 @@
-call npm install --force
+call npm install 
 call .\node_modules\.bin\babel.cmd --presets react,es2015 src/example.jsx --out-file src/example.js
 call  .\node_modules\.bin\babel.cmd --presets react,es2015 test/example.test.jsx --out-file test/example.test.js
