970d87cfe2057fdc0c9223da6d7acb2a765851cf
Matthew Manela <mmanela@gmail.com>
2017-09-15 21:15:44 -0700

    rev

--- Chutzpah/Constants.cs
+++ Chutzpah/Constants.cs
@@ -2,7 +2,7 @@ namespace Chutzpah
 {
     public static class Constants
     {
-        public const string ChutzpahVersion = "4.3.4";
+        public const string ChutzpahVersion = "4.3.5";
 
         public const string TestFileFolder = "ChutzpahTestHarnessFiles";
 
