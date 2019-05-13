221570fe666935e5c2c22f5206e1b23a1f9d5591
Matthew Manela <mmanela@gmail.com>
2019-05-13 14:45:56 -0700

    v4.4.5

--- Chutzpah/Properties/AssemblyInfo.cs
+++ Chutzpah/Properties/AssemblyInfo.cs
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.4.4.0")]
+[assembly: AssemblyVersion("4.4.5.0")]
