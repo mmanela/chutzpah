f058d7544c433f86cc4d12153cd5293a27f9b04d
Matthew Manela <mmanela@gmail.com>
2020-04-16 08:32:55 -0700

    4.4.10

--- ConsoleRunner/Properties/AssemblyInfo.cs
+++ ConsoleRunner/Properties/AssemblyInfo.cs
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.4.9.0")]
+[assembly: AssemblyVersion("4.4.10.0")]
